66eccfc000c0528ecb0b5de67640eb45e378b12e
Thomas Roodson <thomas.roodson@gmail.com>
2025-03-09 22:24:12 -0300

    Update in the class - Smartphone, Nokia and Iphone. Upadate in method abastract and implamentation props.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "983044330", modelo: "`G60", imei: "222222222", memoria: 512);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "983044330", modelo: "`16 Pro MAX", imei: "111111111", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
